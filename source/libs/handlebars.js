590994138a2dad8570a01a63c4c3da66cb40a874
cgutierm <c.gutierrez2511@gmail.com>
2019-10-16 18:09:51 -0300

    avance conexión mysql
    se genera conexión exitosa con mysql y se definen las vistas de las tareas

---
+++ source/libs/handlebars.js
@@ -0,0 +1,16 @@
+const exphbs  = require('handlebars');
+const timeago = require('timeago.js');
+
+exphbs.registerHelper("switch", function(value, options) {
+    this._switch_value_ = value;
+    var html = options.fn(this); // Process the body of the switch block
+    delete this._switch_value_;
+    return html;
+});
+
+exphbs.registerHelper("case", function(value, options) {
+    if (value == this._switch_value_) {
+        return options.fn(this);
+    }
+});
+
